61863ae56429235bae2baf52b5dfef7d97ac248c
Yusuke <waseo228@gmail.com>
2017-06-09 22:45:18 +0000

    Finish user sedtroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :except=>[:show]
+  before_action :admin_user,     only: :destroy
+  
+  def index
+    @users = User.all
+  end
+  
+   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+  
   def show
     @user = User.find(params[:id])
   end
